f5c13a3fb8a9aadbbb6f6737b27b14f4669eaabb
David Olidort <olidortd@gmail.com>
2025-11-23 14:45:49 +0200

    Take URL from appsettings

--- backend/ExpoConnect.Api/Program.cs
+++ backend/ExpoConnect.Api/Program.cs
@@ -10,36 +10,35 @@ using ExpoConnect.Application.Interfaces;
 using ExpoConnect.Domain;
 using ExpoConnect.Infrastructure.Services;
 
-
-
-
 var builder = WebApplication.CreateBuilder(args);
+
+var urls = builder.Configuration["AppUrls"];
+if (!string.IsNullOrWhiteSpace(urls))
+{
+    builder.WebHost.UseUrls(urls);
+}
+
+// DB
 var pwd = builder.Configuration["POSTGRES_PASSWORD"];
 var db = builder.Configuration["POSTGRES_DB"];
 var connectionString = $"Host=localhost;Port=5432;Database={db};Username=postgres;Password={pwd}";
 builder.Configuration["ConnectionStrings:Default"] = connectionString;
 builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("Default"),
-            npg => npg.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
+        npg => npg.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
 
-
-
-
-
+// Services
 builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection(JwtOptions.SectionName));
 builder.Services.AddScoped<IJwtTokenService, JwtTokenService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 
 builder.Services.AddScoped<ICatalogService, CatalogService>();
-
 builder.Services.AddScoped<IUsersService, UsersService>();
-
-builder.Services.AddScoped<ICatalogService, CatalogService>();
-
 builder.Services.AddScoped<IStandsService, StandsService>();
 
 builder.Services.AddControllers();
 
+// Auth
 var jwt = builder.Configuration.GetSection("Jwt");
 var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt["Key"]!));
 
@@ -58,7 +57,6 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             ClockSkew = TimeSpan.FromSeconds(30)
         };
     });
-
 
 // Swagger + Bearer
 builder.Services.AddEndpointsApiExplorer();
@@ -94,7 +92,5 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
-
-app.MapGet("/healthz", () => Results.Ok("OK"));
 
 app.Run();
